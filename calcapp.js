c9a707134b4936c6c378a96b04bb52dbf7390280
Pravesh <praveshkjkj74804@gmail.com>
2025-11-18 12:01:27 +0530

    commit

---
+++ calcapp.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const [calcData, setCalcData] = useState({ num: 0, result: null });
+
+  const doCalc = () => {
+    if (isNaN(calcData.num)) {
+      setCalcData({ ...calcData, result: 'Invalid Number' });
+      return;
+    }
+
+    const num = calcData.num;
+    const sqr = num * num;
+    setCalcData({ ...calcData, result: `Result of ${num} is ${sqr}` });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Square CalcApp</Text>
+      <TextInput
+        value={String(calcData.num)}
+        keyboardType="numeric"
+        placeholder="Enter number"
+        onChangeText={(data) => setCalcData({ ...calcData, num: Number(data) })}
+      />
+      <Button title="Find Square" onPress={doCalc} />
+      <Text>{calcData.result}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
